3cdf5ccba67117160c4ec36018b5839e3c477118
NASMANKAHAN <nasmankhan2001@gmail.com>
2025-02-28 13:03:17 +0000

    add stack fact

--- RecursionBasics1/Racursion.java
+++ RecursionBasics1/Racursion.java
@@ -1,29 +1,38 @@
 package RecursionBasics1;
 
-
 public class Racursion {
 
     // public static void printDec(int n) {
-    //     if (n == 1) {
-    //         System.out.println(n);
-    //         return;
-    //     }
-    //     System.out.println(n + " ");
-    //     printDec(n - 1);
+    // if (n == 1) {
+    // System.out.println(n);
+    // return;
+    // }
+    // System.out.println(n + " ");
+    // printDec(n - 1);
     // }
 
-    public static void printInc(int n){
-        if(n == 1){
+    public static void printInc(int n) {
+        if (n == 1) {
             System.out.println(n + " ");
             return;
         }
-        printInc(n-1);
+        printInc(n - 1);
         System.out.println(n + " ");
     }
 
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int fnm1 = fact(n - 1);
+        int fn = n * fact(n - 1);
+        return fn;
+    }
+
     public static void main(String[] args) {
-        int n = 10;
+        int n = 5;
         // printDec(n);
-        printInc(n);
+        // printInc(n);
+        System.out.println(fact(n));
     }
 }
---
+++ RecursionBasics1/practics.java
@@ -0,0 +1,8 @@
+package RecursionBasics1;
+
+public class practics {
+    public static void main(String[] args) {
+        int n = 5;
+        
+    }
+}
