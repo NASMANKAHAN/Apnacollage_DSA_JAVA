f2106776618e94bea3b56c12946db378d59b0f95
NASMANKAHAN <nasmankhan2001@gmail.com>
2025-03-26 04:24:07 +0000

    New practics add

--- RecursionBasics1/practics.java
+++ RecursionBasics1/practics.java
@@ -79,8 +79,8 @@ public class practics {
         // return x * power(x, n-1);
     }
 
-    public static int optimizedPower(int a, int n) { //0(logn)
-        if(n == 0){
+    public static int optimizedPower(int a, int n) { // 0(logn)
+        if (n == 0) {
             return 1;
         }
         int halfPowerSq = optimizedPower(a, n / 2) * optimizedPower(a, n / 2);
@@ -93,12 +93,74 @@ public class practics {
         return halfPowerSq;
     }
 
+    public static int tilingProblem(int n) {// 2 x n (floor size)
+        // base case
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        // kaam
+        // vertical choice
+        int fnm1 = tilingProblem(n - 1);
+
+        // horizontal choice
+        int fnm2 = tilingProblem(n - 2);
+
+        int totWays = fnm1 + fnm2;
+        return totWays;
+    }
+
+    public static void removeDulicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+        if (idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+
+        // kaam
+        char currChar = str.charAt(idx);
+        if (map[currChar - 'a'] == true) {
+            // duplicate
+            removeDulicates(str, idx + 1, newStr, map);
+        } else {
+            map[currChar - 'a'] = true;
+            removeDulicates(str, idx + 1, newStr.append(currChar), map);
+        }
+    }
+
+    public static void printBinStrings(int n, int lastPlace, String str) {
+        //base case
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+
+
+        // kaam
+        // if(lastPlace == 0){
+        // // sit 0 on chair n
+        // printBinStrings(n - 1, 0, str.append("0"));
+        // printBinStrings(n - 1, 1, str.append("1"));
+        // } else{
+        // printBinStrings(n - 1, 0, str.append("0"));
+        // }
+
+        printBinStrings(n - 1, 0, str+"0");
+        
+        if(lastPlace == 0){
+            printBinStrings(n - 1, 1, str+"1");
+        }
+    }
+
     public static void main(String[] args) {
         int n = 5;
         int arr[] = { 1, 2, 2, 3, 4, 5, 5 };
         int a = 2;
         int b = 5;
-        System.out.println(optimizedPower(a, b));
+        String str = "appnnacollege";
+        // removeDulicates(str, 0, new StringBuilder(""), new boolean[26]);
+        printBinStrings(3, 0, "");
+        // System.out.println(optimizedPower(a, b));
+        // System.out.println(tilingProblem(10));
         // System.out.println(calcSum(n));
         // System.out.println(fib(n));
         // System.out.println(isSorted(arr, 0));
